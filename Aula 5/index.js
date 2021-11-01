63889044e7cd9aaecc916dcfccb844d5cb0a78e0
Luiz Ladeira <luizpauloladeira@hotmail.com>
2021-11-01 13:27:09 -0300

    AULA 05 funções

--- Aula 5/index.js
+++ Aula 5/index.js
@@ -1,3 +1,14 @@
-function Somar(){
-    
+function Somar(x1, x2){
+    return x1+x2;
 }
+
+function calcular(x1, x2, operador){
+    return eval(`${x1} ${operador} ${x2}`);
+}
+
+let resultado = Somar(2,3);
+
+let resultado_calculo = calcular(9,2, "*");
+
+console.log(resultado);
+console.log(resultado_calculo);
