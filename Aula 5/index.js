4c2e3b734e8b984693375cc64f68e03913420b02
Luiz Ladeira <luizpauloladeira@hotmail.com>
2021-11-02 11:34:33 -0300

    aula 05

--- Aula 5/index.js
+++ Aula 5/index.js
@@ -2,13 +2,29 @@ function Somar(x1, x2){
     return x1+x2;
 }
 
+let resultado = Somar(2,3);
+console.log(resultado);
+
+//FUNÇÃO CALCULAR
 function calcular(x1, x2, operador){
     return eval(`${x1} ${operador} ${x2}`);
 }
 
-let resultado = Somar(2,3);
-
 let resultado_calculo = calcular(9,2, "*");
-
-console.log(resultado);
 console.log(resultado_calculo);
+
+//função anonima eu posso invocar ela no momento da criação
+(function (x1, x2, operador){
+    console.log(eval(`${x1} ${operador} ${x2}`));
+})(2,2,"*");
+
+//Arrow_functions
+let subtrair = (x1, x2) => { 
+    return x1-x2;
+}
+
+let resultado_sub = subtrair(6,1);
+console.log(resultado_sub);
+
+
+
